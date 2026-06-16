6a80824c13f63e3cab3c86884bbef838dfdaa25b
FuBangkun <fubangkun2333@outlook.com>
2026-06-17 01:38:51 +0800

    同步上游模板更新，修正MixinBooter

--- src/main/java/com/FuBangkun/tothestarsremake/mixin/LateMixinInit.java
+++ src/main/java/com/FuBangkun/tothestarsremake/mixin/LateMixinInit.java
@@ -1,8 +1,8 @@
 package com.FuBangkun.tothestarsremake.mixin;
 
+import com.FuBangkun.tothestarsremake.Tags;
+import net.minecraftforge.common.ForgeVersion;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import zone.rong.mixinbooter.ILateMixinLoader;
 
 import javax.annotation.Nullable;
@@ -10,10 +10,9 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-@IFMLLoadingPlugin.MCVersion("1.12.2")
-public class TTSREarlyInit implements IFMLLoadingPlugin, ILateMixinLoader {
-    public static final Logger logger = LogManager.getLogger("ToTheStarsRemakeCore");
-
+@IFMLLoadingPlugin.Name(Tags.MOD_ID)
+@IFMLLoadingPlugin.MCVersion(ForgeVersion.mcVersion)
+public class LateMixinInit implements IFMLLoadingPlugin, ILateMixinLoader {
     @Override
     public List<String> getMixinConfigs() {
         return Collections.singletonList("mixins.tothestarsremake.json");
